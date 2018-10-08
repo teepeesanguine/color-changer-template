de49756dba42921edafd82a65d1301886bc4ac0c
cmorgantywls <cmorgan@tywlsbronx.org>
2018-10-07 22:51:30 -0400

    yay

---
+++ colorchanger.js
@@ -0,0 +1,10 @@
+console.log("connected!")
+//always make sure your page connected!
+
+theBody = document.querySelector("body")
+//we can grab tags if they don't have IDs or classes!
+
+//let's make variables for r, g, and b
+//then we will use them to change the background!
+
+//this will be good!
