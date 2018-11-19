7388959f6601fad1406398af9fd77d2af0ef836f
cmorgantywls <cmorgan@tywlsbronx.org>
2018-11-19 14:24:02 -0500

    updating instructions

--- colorchanger.js
+++ colorchanger.js
@@ -4,7 +4,11 @@ console.log("connected!")
 let theBody = document.querySelector("body")
 //we can grab tags if they don't have IDs or classes!
 
+//create a variable called theButton that grabs the button.
+
 //let's make variables for r, g, and b
 //then we will use them to change the background!
 
 //this will be good!
+
+//paste in your getRandomNumber() function below this line so we can use it.
